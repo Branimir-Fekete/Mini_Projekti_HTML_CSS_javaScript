ff809be70351105837907a37d12c2a8bea1eeab9
Branimir-Fekete <branimir.fekete@gmail.com>
2023-10-09 16:45:24 +0200

    Racun

---
+++ Java/RacunOpg/src/main/java/racunopg/model/Racun.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package racunopg.model;
+
+/**
+ *
+ * @author pc
+ */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Racun extends Entitet{
+
+        
+	private BigDecimal PDV;
+	private Date vrijemeIzdavanja;
+	private String opisPlacanja;
+        
+        @ManyToOne
+        private Kupac kupac;
+
+        @ManyToOne
+        private Usluga usluga;
+
+        @ManyToOne
+        private Proizvod proizvod;
+        
+        
+
+	public Racun() {
+	}
+
+	public Racun(BigDecimal PDV, Date vrijemeIzdavanja, String opisPlacanja, int kupacId, int uslugaId,
+		int proizvodId) {
+		this.PDV = PDV;
+		this.vrijemeIzdavanja = vrijemeIzdavanja;
+		this.opisPlacanja = opisPlacanja;
+
+	}
+        
+        
+
+    public BigDecimal getPDV() {
+        return PDV;
+    }
+
+    public void setPDV(BigDecimal PDV) {
+        this.PDV = PDV;
+    }
+
+    public Date getVrijemeIzdavanja() {
+        return vrijemeIzdavanja;
+    }
+
+    public void setVrijemeIzdavanja(Date vrijemeIzdavanja) {
+        this.vrijemeIzdavanja = vrijemeIzdavanja;
+    }
+
+    public String getOpisPlacanja() {
+        return opisPlacanja;
+    }
+
+    public void setOpisPlacanja(String opisPlacanja) {
+        this.opisPlacanja = opisPlacanja;
+    }
+
+    public Kupac getKupac() {
+        return kupac;
+    }
+
+    public void setKupac(Kupac kupac) {
+        this.kupac = kupac;
+    }
+
+    public Usluga getUsluga() {
+        return usluga;
+    }
+
+    public void setUsluga(Usluga usluga) {
+        this.usluga = usluga;
+    }
+
+    public Proizvod getProizvod() {
+        return proizvod;
+    }
+
+    public void setProizvod(Proizvod proizvod) {
+        this.proizvod = proizvod;
+    }
+
+        
+}
---
+++ Java/RacunOpg/src/main/java/racunopg/util/PocetniInsert.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package racunopg.util;
+
+import com.github.javafaker.Faker;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import racunopg.model.Kupac;
+import racunopg.model.Proizvod;
+import racunopg.model.Racun;
+import racunopg.model.Usluga;
+
+/**
+ *
+ * @author pc
+ */
+public class PocetniInsert {
+    
+    public static final int BROJ_PROIZVODA=10;
+    public static final int BROJ_USLUGA=10;
+    public static final int BROJ_KUPACA=10;
+
+    
+    
+    private Faker faker;
+    private Session session;
+    private List<Usluga>usluge;
+    private List<Proizvod>proizvodi;
+    private List<Kupac>kupci;
+    
+    
+    public PocetniInsert() {
+    faker = new Faker();
+    session = HibernateUtil.getSession();
+    usluge = new ArrayList<>();
+    proizvodi = new ArrayList<>();
+    kupci = new ArrayList<>();
+    session.getTransaction().begin();
+    kreirajProizvode();
+    kreirajUsluge();
+    kreirajKupce();
+    
+    
+    
+    session.getTransaction().commit();
+    }
+
+    private void kreirajProizvode() {
+        
+        Proizvod p;
+        for(int i=0;i<BROJ_PROIZVODA;i++){
+            p=new Proizvod();
+            p.setNaziv(faker.beer().name());
+            p.setCijena(new BigDecimal(faker.number().numberBetween(50, 1000)));
+            p.setKolicina(new BigDecimal(faker.number().numberBetween(1, 300)));
+            p.setOpis(faker.lorem().sentence(10));
+            session.persist(p);
+            proizvodi.add(p);
+        }
+        
+    }
+    
+    private void kreirajUsluge(){
+        
+        Usluga u;
+        for(int i=0;i<BROJ_USLUGA;i++){
+            u=new Usluga();
+            u.setNaziv(faker.beer().name());
+            u.setCijenaPoHa(new BigDecimal(faker.number().numberBetween(50, 100)));
+            u.setKolicina(new BigDecimal(faker.number().numberBetween(1, 500)));
+            u.setOpis(faker.lorem().sentence(10));
+            session.persist(u);
+            usluge.add(u);
+        }
+            
+    }
+    
+    private void kreirajKupce(){
+        Kupac k;
+        for(int i=0;i<BROJ_KUPACA;i++){
+            k=new Kupac();
+            k.setNazivSubjekta(faker.company().name());
+            k.setIBAN(faker.business().creditCardNumber());
+            k.setOIB(Alati.getOib());
+            k.setAdresa(faker.address().fullAddress());
+            session.persist(k);
+            kupci.add(k);
+        }
+    }
+    
+    private void kreirajRacune(){
+        Racun r;
+        
+    }
+    
+}
